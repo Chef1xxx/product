5b150f341333ff085a1bcbf40a26ab0fe661001c
Chef1xxx <Chef1x@yandex.ru>
2025-04-07 02:00:41 +0700

    крайнее исправление:)

--- index.php
+++ index.php
@@ -1,30 +1,48 @@
 <?php require_once("utils/functions.php") ?>
 <?php require_once("templates/header.php") ?>
-<?php 
+<?php
+if (isset($_GET['delete_id'])) {
+  deleteProduct($_GET['delete_id']);
+  header("Location: index.php");
+  exit();
+}
 
-?> 
+$products = getAllProducts();
+?>
 
-<table class="table table-hover table-bordered" style="margin-top: 45px;">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Название</th>
-      <th scope="col">Категория</th>
-      <th scope="col">Артикул</th>
-      <th scope="col">Цена</th>
-      <th scope="col">Цена со скидкой</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Меф</td>
-      <td>ББ</td>
-      <td>7839101</td>
-      <td>10р</td>
-      <td>5</td>
-      <td>Удалить</td>
-    </tr>
+<div class="container mt-4">
+  <a href="create.php" class="btn btn-primary mb-3">Add New Product</a>
 
-<?php require_once("templates/footer.php") ?> 
+  <table class="table table-hover table-bordered">
+    <thead class="table-dark">
+      <tr>
+        <th>ID</th>
+        <th>Название</th>
+        <th>Категория</th>
+        <th>Цена</th>
+        <th>Скидка</th>
+        <th>Цена со скидкой</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($products as $product): ?>
+        <tr>
+          <td><?= $product['id'] ?></td>
+          <td><?= $product['name'] ?></td>
+          <td><?= $product['category'] ?></td>
+          <td><?= $product['price'] ?></td>
+          <td><?= $product['discount'] ?></td>
+          <td><?= $product['price'] - $product['discount'] ?></td>
+          <td>
+            <a href="info.php?id=<?= $product['id'] ?>" class="btn btn-info btn-sm">Инфо</a>
+            <a href="edit.php?id=<?= $product['id'] ?>" class="btn btn-warning btn-sm">Редактировать</a>
+            <a href="index.php?delete_id=<?= $product['id'] ?>" class="btn btn-danger btn-sm">Удалить</a>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</div>
+
+<?php require_once("templates/footer.php") ?>
